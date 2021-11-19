cbbec45f37c95cdd6b99329a895d6bb5a6e683a3
Jeniferignatius <jeni.igni@gmail.com>
2021-11-19 14:53:42 +0200

    first commit

---
+++ getMovie.php
@@ -0,0 +1,136 @@
+<?php
+$id = $_GET["id"];
+$valid = false;
+
+if($id == null)
+{
+
+	header('Location: index.php');
+}
+else{
+    $valid = true;
+
+    $url = file_get_contents("http://www.omdbapi.com/?i=$id&apikey=51110af3&plot=short&r=json", true);
+    
+    $json = json_decode($url,true);
+    
+    $movieTitle = $json['Title'];
+    $movie_Genre = $json['Genre'];
+    $movieYear = $json['Year'];
+    $movie_Rated = $json['Rated'];
+    $moviePlot = $json['Plot'];
+    $movie_Poster = $json['Poster'];
+    $movie_actors = $json['Actors'];
+}
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title><?php 
+            echo $movieTitle;?> | Info
+        </title>
+        <?php 
+        include_once("styles.php");
+        ?>
+    </head>
+
+    <body>
+        <?php 
+            if($valid == true):?>
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <img class="heroImg" src="<?php echo $movie_Poster ?>">
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-md-12">
+                        <h1 class="heroHeader">
+                        <?php
+                            echo $movieTitle;?>
+                        </h1>
+                    </div>
+                </div>
+
+                <section class="infoContainer">
+                    <table>
+                        <tr>
+                            <th>
+                                <p>ID</p>
+                            </th>
+                            <td>
+                                <p>
+                                    <?php echo $id;?>
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                               
+                                <p>Genre</p>
+                            </th>
+                            <td>
+                                <p>
+                                    <?php echo $movie_Genre;?>
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <p>Year</p>
+                            </th>
+                            <td>
+                                <p>
+                                    <?php echo $movieYear;?>
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <p>Rated</p>
+                            </th>
+                            <td>
+                                <p>
+                                    <?php echo $movie_Rated;?>
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <p>Plot</p>
+                            </th>
+                            <td>
+                                <p>
+
+                                    <?php echo $moviePlot;?>
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="lastTD">
+                                <p>Actors</p>
+                            </th>
+                            <td class="lastTD">
+                                <p>
+                                    <?php echo $movie_actors;?>
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                    <div class="row">
+                        <div class="col-md-12 backButton">
+                            <a href="index.php">Back</a>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+
+            <?php endif;?>
+    </body>
+
+    </html>
---
+++ index.php
@@ -0,0 +1,138 @@
+<?php
+
+$valid = false;
+$error = "";	
+
+if(isset($_POST["submit"])){
+    
+    $name = $_POST["search"];
+    
+    if(empty($name))
+    {
+        
+        $error = " Please Enter A Search Term";
+    }
+    else{
+        
+        $nameString = str_replace(" ", "%20", $name);
+        
+        if(substr($nameString,-3) == "%20"){
+          
+          $name = chop($nameString, substr($nameString,-3)); 
+         
+        }
+        else
+        {
+           $name = $nameString; 
+        }
+        
+        $url = file_get_contents("http://www.omdbapi.com/?s=$name&apikey=51110af3&y=&plot=short&r=json", true);
+        
+        $json = json_decode($url);
+        
+        $jsonError = json_decode($url,true);
+        
+        if($jsonError['Response'] == "False"){
+            
+            if($jsonError['Error'] == "Movie not found!")
+            {
+                
+                $error = "Searched Movie Not Found!";
+            }
+            else
+            {
+                $error = "Please Enter A valid Search Term";
+            }
+        }
+        else
+        {
+            sort($json->Search);
+            $valid = true;
+        }
+
+    }
+}
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset "UTF-8"/>
+        <title>Movie Search</title>
+        <?php include_once("styles.php");?>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Movie Search<i class="fa fa-search" aria-hidden="true">&nbsp;</i></h1>
+                <form name="movieSearch" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+                    <label id="errorMessage" class="errMsg">
+                        <?php echo "$error";?>
+                    </label>
+                    <img src ="poster.jpg">
+                    <input id="search" type="text" name="search" placeholder="Search..."  />
+                    <input id="submitButton" type="submit" name="submit" value="submit">
+                </form>
+            </div>
+        </div>
+
+        <?php if($valid == true): ?>
+            <div class="container">
+                <div class="tbl-header">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>IMDB ID</th>
+                                <th>Title</th>
+                                <th>Genre</th>
+                                <th>Year Released</th>
+                                <th>Poster</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+                <div class="tbl-content">
+                    <table>
+                        <tbody>
+
+                            <?php foreach ($json->Search as $movie): ?>
+                                <tr>
+                                    <td class="imdbID">
+                                        <?php
+                                            echo "<a href=\"getMovie.php?id=$movie->imdbID\">$movie->imdbID</a>";
+                                        ?></td>
+                                    <td>
+                                        <?php 
+                                            echo $movie->Title; 
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php 
+                                        
+                                            echo $movie->Type; 
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php 
+                                       
+                                            echo $movie->Year; 
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php 
+                                            echo "<img src=\"$movie->Poster\"";
+                                        ?>
+                                    </td>
+                                </tr>
+
+                                <?php endforeach;?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <?php endif;?>
+    </body>
+
+    </html>
