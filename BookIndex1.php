f250f05db7af1a590f30c02b2338e88eb282d49f
Jeniferignatius <jeni.igni@gmail.com>
2021-11-24 10:56:40 +0200

    book search index added

---
+++ BookIndex1.php
@@ -0,0 +1,142 @@
+<?php
+
+$valid = false;
+$error = "";	
+
+if(isset($_POST["submit"])){
+    
+    $name = $_POST["search"];
+    
+    if(empty($name))
+    {
+        
+        $error = " Please Enter A Search Term";
+    }
+    else{
+        
+        $nameString = str_replace(" ", "%20", $name);
+        
+        if(substr($nameString,-3) == "%20"){
+          
+          $name = chop($nameString, substr($nameString,-3)); 
+         
+        }
+        else
+        {
+           $name = $nameString; 
+        }
+        
+        //$url = file_get_contents("http://openlibrary.org/api/books?bibkeys=ISBN:'.$name.'&format=json&jscmd=data", true);
+        $url = file_get_contents("https://openlibrary.org/search.json?title=$name", true);
+        
+        $json = json_decode($url);
+        
+        $jsonError = json_decode($url,true);
+        //echo "<pre>";
+        //print_r($json);
+        //echo "</pre>";
+     
+        
+        if($jsonError['Response'] == "False"){
+            
+            if($jsonError['Error'] == "book not found!")
+            {
+                
+                $error = "Searched book Not Found!";
+            }
+            else
+            {
+                $error = "Please Enter A valid Search Term";
+            }
+        }
+        else
+        {
+            sort($json->docs);
+            $valid = true;
+        }
+
+    }
+}
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset "UTF-8"/>
+        <title>Book Search</title>
+        <?php include_once("styles.php");?>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Book Search<i class="fa fa-search" aria-hidden="true">&nbsp;</i></h1>
+                <form name="bookSearch" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+                    <label id="errorMessage" class="errMsg">
+                        <?php echo "$error";?>
+                    </label>
+                    <input id="search" type="text" name="search" placeholder="Search..."  />
+                    <input id="submitButton" type="submit" name="submit" value="submit">
+                </form>
+            </div>
+        </div>
+
+        <?php if($valid == true): ?>
+            <div class="container">
+                <div class="tbl-header">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Key</th>
+                                <th>ISBN</th>
+                                <th>Title</th>
+                                <th>Author_name</th>
+                                <th>Publish_year</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+                <div class="tbl-content">
+                    <table>
+                        <tbody>
+
+                            <?php foreach ($json->docs as $book): ?>
+                                <tr>
+                                    <td class="imdbID">
+                                        <?php
+                                            echo "<a href=\"getBook1.php?id=$book->key\">$book->key</a>";
+                                        ?></td>
+                                    <td>
+                                        <?php 
+                                            echo $book->isbn[0]; 
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php 
+                                        
+                                            echo $book->title; 
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php 
+                                       
+                                            echo $book->author_name[0]; 
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php 
+                                            echo $book->publish_year[0]; 
+                                        ?>
+                                    </td>
+                                </tr>
+
+                                <?php endforeach;?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <?php endif;?>
+    </body>
+
+    </html>
